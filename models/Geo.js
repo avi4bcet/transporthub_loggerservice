50e9f01793532e974b98d2c042402450beebbf74
Dasgupta <avishek.a.dasgupta@capgemini.com>
2018-09-15 02:39:50 +0530

    The changes in structure

---
+++ models/Geo.js
@@ -0,0 +1,28 @@
+var db=require('../dbconnection');
+
+
+var Task={
+
+
+
+getGeoLog:function(Task,callback) {
+//console.log(SELECT count(id) FROM tbl_users where email=avi4bcet@gmail.com AND password=Pass@1234);
+
+
+
+
+
+},
+
+
+//getAllPartDetails:function(callback){
+
+//return db.query("SELECT * FROM tbl_part_type,tbl_part_details,tbl_bin where tbl_part_type.id=tbl_part_details.part_id AND tbl_bin.id=tbl_part_details.binname ORDER BY tbl_part_details.id DESC",callback);
+
+//},
+
+
+
+
+};
+module.exports=Task;
